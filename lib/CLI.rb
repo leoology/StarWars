e80f4c55aa63057255d6b9372932905d3c36c860
Leo <efrenn98@gmail.com>
2020-12-05 15:41:18 -0800

    Adding a few character functions to be called on in my cli

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -4,11 +4,43 @@ require 'area'
 
 class CommandLineInterface
     def menu
+        @characters = Character.list_characters
         puts "Enter the number of the character on which you'd like more info."
         list_characters 
         input=gets.strip.downcase 
-        if input.to_i >0
-            
+        case input
+        when "1"
+            puts "More info on Luke Skywalker"
+            Character.luke
+        when "2"
+            puts "More info on Darth Vader"
+            Character.darth_vader
+        when "3"
+            puts "More info on Yoda"
+            Character.yoda
+        when "4"
+            puts "More info on Chewbacca"
+            Character.chewie
+        when "5"
+            puts "More info on Obi-Wan Kenobi"
+            Character.obi_wan
+        when "6"
+            puts "More info on Han Solo"
+            Character.han_solo
+        when "7"
+            puts "More info on General Grievous"
+            Character.grievous
+        when "8"
+            puts "More info on Annakin Skywalker"
+            Character.annie
+        when "9"
+            puts "More info on Qui-Gon Jinn"
+            Character.man_who_got_skunked_by_darth_maul
+        when "10"
+            puts "More info on Darth Maul"
+            Character.maul
+        else 
+            puts "You can also type 'exit'"
         end 
     end
     
--- lib/characters.rb
+++ lib/characters.rb
@@ -10,16 +10,100 @@ class Character
     
 
     def list_character
-        characters= 1..3
+        characters=[]
     characters.each do |num| 
         jedi= StarWarsAPI.new.get_sw_http(url(num))
        #homeworld = StarWarsAPI.new.get_sw_http(jedi['homeworld'])
-        puts "Name: #{people['name']}"
-        puts "Born: #{people['birth_year']}"
+        puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}"
+
        # puts "Homeworld: #{b'name']}"
-    binding.pry
-    end
-end 
+
+        end
+        #binding.pry
+    end 
  
-end 
+    def luke
+        jedi= StarWarsAPI.new.get_sw_http(url(1))
+        puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}"
+        binding.pry 
+    end
+
+    def darth_vader
+        jedi= StarWarsAPI.new.get_sw_http(url(4))
+        puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def yoda
+        jedi= StarWarsAPI.new.get_sw_http(url(20))
+        puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def chewie
+        jedi= StarWarsAPI.new.get_sw_http(url(13))
+        puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def obi_wan
+        jedi= StarWarsAPI.new.get_sw_http(url(10))
+        puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+        end
+
+    def han_solo
+        jedi= StarWarsAPI.new.get_sw_http(url(14))
+         puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def grievous
+        jedi= StarWarsAPI.new.get_sw_http(url(79))
+         puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def annie
+        jedi= StarWarsAPI.new.get_sw_http(url(11))
+         puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def darth_vader
+        jedi= StarWarsAPI.new.get_sw_http(url(4))
+         puts "Name: #{jedi['name']}"
+        puts "Born: #{jedi['birth_year']}"
+        puts "Height: #{jedi['height']}"
+        puts "Gender: #{jedi['gender']}" 
+    end
+
+    def man_who_got_skunked_by_darth_maul
+        jedi= StarWarsAPI.new.get_sw_http(url(11))
+        puts "Name: #{jedi['name']}"
+       puts "Born: #{jedi['birth_year']}"
+       puts "Height: #{jedi['height']}"
+       puts "Gender: #{jedi['gender']}" 
+   end   
+end
 Character.new.list_character
